906a3c02398b7a9a34e48e33f6c86c8bd838d172
sotomaque <sotomaque@gmail.com>
2020-04-15 17:34:26 -0700

    created new package for custom exception handling, taking care of the scenario when the input passes our validator reuqirements at the service layer, yet fails at the db later (i.e. unique project id scenario);

--- ppmtool-java/src/main/java/com/sotomaque/ppmtool/services/ProjectService.java
+++ ppmtool-java/src/main/java/com/sotomaque/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.sotomaque.ppmtool.services;
 
 import com.sotomaque.ppmtool.domain.Project;
+import com.sotomaque.ppmtool.exceptions.ProjectIdException;
 import com.sotomaque.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
     }
 }
