12a9371c73b4345879397d6000d7556a1253f5ea
sotomaque <sotomaque@gmail.com>
2020-04-23 06:21:31 -1000

    added JWT Provider prework; moved strings such as header text and token prefix into a Security Config file; in UserRepository added the ability to findUserByUsername and getUserById; created Payload Package to house LoginRequest and JWTLoginSuccessResponse; created our UserDetailService to actually find user when attempting to login

--- ppmtool-java/src/main/java/com/sotomaque/ppmtool/security/SecurityConfig.java
+++ ppmtool-java/src/main/java/com/sotomaque/ppmtool/security/SecurityConfig.java
@@ -1,12 +1,21 @@
 package com.sotomaque.ppmtool.security;
 
+import com.sotomaque.ppmtool.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static com.sotomaque.ppmtool.security.SecurityConstants.H2_URL;
+import static com.sotomaque.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +28,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // authentication manager builder is used for DB Authentication and
+    // our authentication provider -> Builds Authentication Manager
+
+    // Authentication Manager is what we use to generate token for a user
+    // that is Authenticated
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    // Create Bean for Authentication Manager
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -46,7 +78,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js"
                 ).permitAll()
-                .antMatchers("/api/users/**").permitAll()
+                .antMatchers(SIGN_UP_URLS).permitAll()
+                .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
     }
 }
